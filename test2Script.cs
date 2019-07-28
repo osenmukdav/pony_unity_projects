99c43611e430e56ea25bcae9c3f8335d0726317a
osenmukdav <47229463+osenmukdav@users.noreply.github.com>
2019-07-28 22:58:38 +0300

    Add files via upload

---
+++ test2Script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test2Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Quaternion q = Input.gyro.attitude;
+        float temp = q.z;
+        q.z = q.y;
+        q.y = temp;
+        gameObject.transform.rotation = q;
+    }
+}
